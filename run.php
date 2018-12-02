312544127e2670c3765d2b573d79c889a376e7f3
erienniu <97516719@qq.com>
2018-12-02 18:43:02 +0800

    checkout redis

--- run.php
+++ run.php
@@ -126,6 +126,7 @@ $messageHandler->setHandler(function ($message) {
     if ($message['type'] == 'emoticon'){
         if (isset($emotion_mode[$message['from']['UserName']])){
             Text::send($message['from']['UserName'], $message['raw']);
+            echo $message['raw'];
 //            Emoticon::download($message);
 //            $emotion_mode[$message['from']['UserName']] = Multimedia::fileName($message).".gif";
         }
